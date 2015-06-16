b47c03dc2d916fa36dd3cea1fe954adbf67ed84a
Trung Vuong <trung.vuong@vndirect.com.vn>
2015-06-16 18:30:49 +0700

    added TODO tag

--- services/bps/client/online_trading.rb
+++ services/bps/client/online_trading.rb
@@ -2,6 +2,7 @@ require_relative './base'
 
 class BPS::Client::OnlineTrading < BPS::Client::Base
   def initialize
+    # TODO make it a config
     super 'http://10.26.53.91/BOProxyServiceNew/OnlineTradingService.asmx?wsdl'
   end
 end
